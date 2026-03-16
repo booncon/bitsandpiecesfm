fb16980c6131bafede5b34eaf558dc1820c66bb8
sftsk <luki@booncon.com>
2026-03-16 14:16:29 +0200

    fix sitemap build

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import Sitemap from 'vite-plugin-sitemap';
 import { defineConfig } from 'vite';
+import { mkdirSync } from 'fs';
 
 const siteUrl = (process.env.SITE_URL ?? 'https://bitsandpieces.fm').replace(/\/$/, '');
 const basePath = (process.env.BASE_PATH ?? '').replace(/\/$/, '');
@@ -12,6 +13,12 @@ export default defineConfig({
   plugins: [
     enhancedImages(),
     sveltekit(),
+    {
+      name: 'ensure-build-dir',
+      closeBundle() {
+        mkdirSync('build', { recursive: true });
+      }
+    },
     Sitemap({
       hostname,
       outDir: 'build',
